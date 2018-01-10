6319e667a0045ff7b9cd5b7c6f63427c0ced7a65
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-01-10 17:33:22 -0500

    enter

--- intro.js
+++ intro.js
@@ -35,33 +35,20 @@ $(document).ready(function(){
 
 
 //smooth scroll code 
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
+$("a[href*='#']:not(a[href='#'])").click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      || location.hostname == this.hostname) {
+
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { 
-            return false;
-          } else {
-            $target.attr('tabindex','-1');
-            $target.focus(); 
-          };
-        });
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+               scrollTop: target.offset().top
+          }, 1000);
+          return false;
       }
-    }
-  });
+  }
+});
+
 
   
